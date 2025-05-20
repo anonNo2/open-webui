a7197add8e10bff513f668541b19dcb32f64adc7
anonNo2 <774320139@qq.com>
2025-05-20 17:24:33 +0800

    change start script

--- backend/start.sh
+++ backend/start.sh
@@ -2,6 +2,7 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "$SCRIPT_DIR" || exit
+export CORS_ALLOW_ORIGIN='*'
 
 # Add conditional Playwright browser installation
 if [[ "${WEB_LOADER_ENGINE,,}" == "playwright" ]]; then
